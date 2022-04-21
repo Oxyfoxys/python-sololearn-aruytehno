2420dce99f67d0c3ce63fb33508df389cbcb04dd
aruytehno <aruytehno@gmail.com>
2022-04-21 18:30:23 +0300

    Update basic_concepts.py

--- com/skillbox/PythonCore/basic_concepts.py
+++ com/skillbox/PythonCore/basic_concepts.py
@@ -43,7 +43,8 @@ There are 68 children in the group, and each child should get 2 scoops of ice cr
 Task
 Write a program to calculate and output the total number of ice cream scoops we need.
     """
-    print(simple_operations.__doc__)
+    print(brain_freeze.__doc__)
+    print('#your code goes here')
     print(2 + 2)
 
 
@@ -58,14 +59,60 @@ Floats
     print(0.42)
 
 
+def exponentiation():
+    """
+4.1 Lesson
+Exponentiation
+    """
+
+    print(floats.__doc__)
+    print(2 ** 5)
+    print(9 ** (1 / 2))
+
+
+def quotient_and_remainder():
+    """
+5.1 Lesson
+Quotient & Remainder
+    """
+
+    print(quotient_and_remainder.__doc__)
+    print('20 // 6 =', 20 // 6)
+    print('9 ** (1 / 2) =', 9 ** (1 / 2))
+    print('20 % 6 =', 20 % 6)
+    print('1.25 % 0.5 =', 1.25 % 0.5)
+
+
+def quotient():
+    """
+5.2 Practice
+Quotient
+
+Calculate and output the number of miles in 1000 kilometers.
+
+Hint
+One mile is 1.6 kilometers, so find the quotient of 1000 and 1.6.
+    """
+    print(quotient.__doc__)
+    print('#your code goes here')
+    print('1000 // 1.6 =', 1000 // 1.6)
+
+
 if __name__ == '__main__':
-    # 1.1
-    welcome_to_python()
-    # 1.2
-    your_first_program()
-    # 2.1
-    simple_operations()
-    # 2.2
-    brain_freeze()
-    # 3.1
-    floats()
+    # # 1.1
+    # welcome_to_python()
+    # # 1.2
+    # your_first_program()
+    # # 2.1
+    # simple_operations()
+    # # 2.2
+    # brain_freeze()
+    # # 3.1
+    # floats()
+    # # 4.1
+    # exponentiation()
+    # 5.1
+    quotient_and_remainder()
+    # 5.2
+    quotient()
+    # 6.1
