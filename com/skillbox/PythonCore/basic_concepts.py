ebe95b35d29239d95e58e4fb772fb1d30d93e14c
aruytehno <aruytehno@gmail.com>
2022-04-21 16:55:28 +0300

    Added structure

---
+++ com/skillbox/PythonCore/basic_concepts.py
@@ -0,0 +1,16 @@
+# 1.1 Урок
+# вывод текста
+def hello_world():
+    print("Hello World!")
+
+
+# 2.1 Урок
+# Простые операции
+
+def simple_operations():
+    print(2 + 2)
+
+
+if __name__ == '__main__':
+    hello_world()
+    simple_operations()
