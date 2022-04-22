0f8affc60dbb6ccb4c3e28d29210da7c19e32b99
aruytehno <aruytehno@gmail.com>
2022-04-22 14:07:56 +0300

    Added new lesson

---
+++ com/skillbox/PythonCore/strings_and_variables.py
@@ -0,0 +1,31 @@
+def strings():
+    """
+8.1 Lesson
+Strings
+    """
+    print(strings.__doc__)
+    print('print:', 'Python is fun!')
+    print('print:', 'Always look on the bright side of life')
+    print('print:', 'Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+
+
+def i_code():
+    """
+8.2 Practice
+Strings
+
+Fix the given code to output I'm a programmer.
+
+Hint
+Escape the single quote inside the string.
+    """
+    print(i_code.__doc__)
+    print('#your code goes here')
+    print('print:', 'I\'m a programmer')
+
+
+if __name__ == '__main__':
+    # 8.1
+    strings()
+    # 8.2
+    i_code()
